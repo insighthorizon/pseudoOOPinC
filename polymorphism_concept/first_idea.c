3a7ef2f24d8f2fa2fb753cac4978822234880413
insighthorizon <majiricek@seznam.cz>
2018-04-09 22:44:02 +0200

    tested polymorphism concept *first idea* - repaired errors

--- polymorphism_concept/first_idea.c
+++ polymorphism_concept/first_idea.c
@@ -1,10 +1,15 @@
-#include <stdio.>
+#include <stdio.h>
 
 typedef struct{ /* Float polytype definition */
   int type_identifier; /* there will be asigned value to identify data type */
   float data;
   } float_poly_t;
-  
+typedef struct{
+  int type_identifier;
+  float data[64];
+  } f64array_poly_t;
+
+
 /* This should be implemented for all data types including all types of arrays and structures */
   #define CREATE_FLOAT(NAME, INIT_VALUE) \
   float_poly_t NAME = (float_poly_t){0x00000001, INIT_VALUE};
@@ -21,7 +26,8 @@ int read_something(int *something, int *output)
   {
     if((*output) ==0x0000001)
     {
-    ((float_poly_t*)output)->data = ((float_poly_t*)something)->data;
+      ((float_poly_t*)output)->data = ((float_poly_t*)something)->data;
+    return 0;
     }
   }
   printf("error!\n");
@@ -30,10 +36,10 @@ int read_something(int *something, int *output)
 
 int main(void)
 {
-   CREATE_FLOAT(myFloat, 10)
-   CREATE_FLOAT(x, 0)
-  read_something(&myFloat, &x); 
-  printf("%f\n", x);
+  CREATE_FLOAT(myFloat, 10.864)
+  CREATE_FLOAT(x, 0.8)
+  volatile int result =  read_something((&myFloat), (&x)); 
+  printf("%f\n", x.data);
    
   return 0;
 }
