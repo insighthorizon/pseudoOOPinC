d40ec2c69017ade075d1e76bd50b620cb022c358
insighthorizon <majiricek@seznam.cz>
2018-04-09 09:53:33 +0200

    Create first_idea.c

---
+++ polymorphism_concept/first_idea.c
@@ -0,0 +1,9 @@
+typedef struct{ /* Float polytype definition */
+  int type_identifier; /* there will be asigned value to identify data type */
+  float data;
+  } float_poly_t;
+  
+  #define CREATE_INT(INIT_VALUE) \
+  float_poly_t
+  
+  /* TODO macro CREATE_POLYVAR(TYPE, NAME, INIT_VALUE) */
